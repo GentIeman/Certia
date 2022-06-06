9dd1784f0c49b3d69f628a0408d4099f5c8d4fc7
gentIeman <gentleman-2021@mail.ru>
2022-06-06 20:49:41 +0300

    feat: a feedback page has been created

---
+++ static/scripts/modal.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector(".open-modal")
+const modal = document.querySelector(".modal")
+const form = document.querySelector(".form-submit")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    modal.showModal()
+}, true)
+
+modal.addEventListener("click", (event) => {
+    if (event.target === event.currentTarget) modal.close()
+}, false)
