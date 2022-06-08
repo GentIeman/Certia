2813eb0eaec58c5c96bcb1657c2a4d97de82b8a5
gentIeman <gentleman-2021@mail.ru>
2022-06-08 11:28:16 +0300

    feat: account page created

--- static/scripts/transfer.js
+++ static/scripts/transfer.js
@@ -2,12 +2,15 @@ const transfer = document.querySelector(".modal.transfer")
 const transferForm = document.querySelector(".transfer__form")
 const btnTransferForm = document.querySelector(".operation__btn-transfer")
 const successfulTransfer = document.querySelector(".successful-transfer")
+const cardField = document.querySelector(".transfer__input_card")
+const amountField = document.querySelector(".transfer__input_amount")
 
-const maxlengthForInputNumber = (value, maxlength) => {
-    if (value.length > maxlength) value = value.slice(0, maxlength);
-    return value
-}
+const inputMaxlength = (value, maxlength) => value = (value.length > maxlength) ? value.slice(0, maxlength) : value
 
+const onlyNumber = (value) => value = value.replace(/\D/g, "")
+
+cardField.addEventListener("input", (event) => event.target.value = inputMaxlength(event.target.value, 16))
+amountField.addEventListener("input", (event) => event.target.value = onlyNumber(event.target.value))
 btnTransferForm.addEventListener("click", () => transfer.showModal())
 
 transferForm.addEventListener("submit", (event) => {
