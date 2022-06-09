ee6eb6e72405e26520c0ce9350c44b407913752c
gentIeman <gentleman-2021@mail.ru>
2022-06-09 13:21:23 +0300

    update: a function for closing modal windows has been created

--- static/scripts/transfer.js
+++ static/scripts/transfer.js
@@ -8,6 +8,12 @@ const amountField = document.querySelector(".transfer-modal__input_amount")
 const inputMaxlength = (value, maxlength) => value = (value.length > maxlength) ? value.slice(0, maxlength) : value
 const onlyNumber = (value) => value = value.replace(/\D/g, "")
 
+function closeModal(modal){
+    modal.addEventListener("click", (event) => {
+        if (event.target === event.currentTarget) modal.close()
+    })
+    return modal
+}
 
 cardField.addEventListener("input", (event) => event.target.value = inputMaxlength(event.target.value, 16))
 amountField.addEventListener("input", (event) => event.target.value = onlyNumber(event.target.value))
@@ -19,5 +25,5 @@ transferForm.addEventListener("submit", (event) => {
     transfer.close()
 }, false)
 
-successfulTransfer.addEventListener("click", () => closeModal(successfulTransfer))
-transfer.addEventListener("click", () => closeModal(transfer))
+successfulTransfer.addEventListener("click", closeModal(successfulTransfer))
+transfer.addEventListener("click", closeModal(transfer))
