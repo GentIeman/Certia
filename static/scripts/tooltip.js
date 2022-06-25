29947eb42ffcd68e28ffbf886ca20dd9399d7ace
gentIeman <gentleman-2021@mail.ru>
2022-06-25 14:00:42 +0300

    deleted

--- static/scripts/tooltip.js
+++
@@ -1,5 +0,0 @@
-const tooltip = document.querySelector(".tooltip-wrap")
-
-tooltip.addEventListener("click", event => {
-    if (event.target === event.currentTarget) tooltip.classList.remove("tooltip-wrap_show")
-})
