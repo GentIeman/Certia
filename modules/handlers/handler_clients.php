b9bc326a6883937e66a506fa676e8cd5e3902973
GentIeman <ilyashepelev23@gmail.com>
2022-11-29 19:18:04 +0300

    update: added action - transaction

--- modules/handlers/handler_clients.php
+++ modules/handlers/handler_clients.php
@@ -4,5 +4,8 @@ if (isset($_GET["action"])) {
         case "send-feedback":
             require_once "./modules/clients/send_feedback.php";
             break;
+        case "transaction":
+            require_once "./modules/clients/transaction.php";
+            break;
     }
 }
