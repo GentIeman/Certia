f6d5b38d547f5f1d6c7795f628e77189c736de09
GentIeman <ilyashepelev23@gmail.com>
2022-11-29 19:18:57 +0300

    update: added pages containing information about account and transaction

--- index.php
+++ index.php
@@ -50,7 +50,15 @@ if (isset($_GET["page"])) {
             require_once "./modules/clients/getting_client_info.php";
             require_once "./views/profile.php";
             break;
+        case "account-info":
+            require_once "./modules/accounts/getting_account_info.php";
+            require_once "./views/account_info.php";
+            break;
+        case "transaction-info":
+            require_once "./modules/handlers/handler_transaction.php";
+            require_once "./views/transaction_info.php";
+            break;
+        case "logout":
+            require_once "./modules/auth/logout.php";
     }
-} else {
-    require_once "./views/login.php";
 }
